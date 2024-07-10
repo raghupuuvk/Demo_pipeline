54ea51f501922063a91cdc74bfba4966db0f29a4
raghupuuvk <raghupuuvk20@gmail.com>
2024-07-10 22:36:28 +0530

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.50.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+ region = "us-east-1"
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
